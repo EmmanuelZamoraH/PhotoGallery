63fbeb479c0d73d4f8492940678d74a5ee31369f
Emmanuel <zamora.emmanuel.1hm@gmail.com>
2023-05-19 20:57:37 -0500

    Ya jalaaaaaaa

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,9 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-  }
+  },
+  optimizeDeps:{
+    exclude:['@ionic/pwa-elements/loader'],
+  },
 })
+
